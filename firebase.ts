737b72dc725a77df8905d108418db2a1681c4113
Royce Williams <roycewilliamsj@gmail.com>
2025-03-21 09:17:46 -0700

    config file

--- firebase.ts
+++ firebase.ts
@@ -1,34 +1,44 @@
+"use client"
 // Import the Firebase functions you need
-import { initializeApp, FirebaseApp } from "firebase/app";
+import { initializeApp, FirebaseApp, getApps, getApp } from "firebase/app";
 import { getAnalytics, Analytics } from "firebase/analytics";
 import { getStorage, FirebaseStorage } from "firebase/storage";
-import { getAuth } from "firebase/auth";
+import { Auth, getAuth } from "firebase/auth";
 
 // Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: "from-royce.firebaseapp.com",
-  projectId: "from-royce",
-  storageBucket: "from-royce.firebasestorage.app",
-  messagingSenderId: "1042271682270",
-  appId: "1:1042271682270:web:1a7ecfa216621cce3b4c4f",
-  measurementId: "G-L3241H26V3"
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 console.log("Firebase API KEY:", process.env.NEXT_PUBLIC_FIREBASE_API_KEY);
 
 
 // Initialize Firebase
-const app: FirebaseApp = initializeApp(firebaseConfig);
-const storage: FirebaseStorage = getStorage(app);
-const auth = getAuth(app);
-
-
-// Ensure Analytics is only initialized in the browser
+let app: FirebaseApp;
+let auth: Auth | undefined;
+let storage: FirebaseStorage | undefined;
 let analytics: Analytics | undefined;
-if (typeof window !== "undefined") {
+
+if (typeof window !== 'undefined') {
+  if (!getApps().length) {
+    app = initializeApp(firebaseConfig);
+  } else {
+    app = getApp();
+  }
+
+  auth = getAuth(app);
+  storage = getStorage(app);
   analytics = getAnalytics(app);
 }
+
+
+
 
 // Export Firebase services
 export { storage, analytics, auth };
