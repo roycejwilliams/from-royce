63b87d6a8a5e6be8eb41b3bfa495323268b1121c
Royce Williams <roycewilliamsj@gmail.com>
2025-03-24 18:33:18 -0700

    update

--- functions/index.js
+++ functions/index.js
@@ -1,23 +1,9 @@
-require("dotenv").config();
-const { initializeApp } = require("firebase/app");
-
-
 const { onRequest } = require('firebase-functions/v2/https');
 const express = require("express");
 const cors = require("cors");
 const next = require("next");
 const { Pool } = require("pg");
 
-
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-};
-
-initializeApp(firebaseConfig);
 
 // PostgreSQL connection
 const pool = new Pool({
