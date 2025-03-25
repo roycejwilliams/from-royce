61ed460a0fb19342631299ce5ef21584aef70e4e
Royce Williams <roycewilliamsj@gmail.com>
2025-03-24 21:25:18 -0700

    try again

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,27 +1,22 @@
 "use client";
-import { useEffect } from "react";
 import "../styles/globals.css";
-import TransitionProvider from "../context/TransitionContext";
-import Transition from "../components/Transition";
+import TransitionProvider from '../context/TransitionContext'
+import Transition from '../components/Transition';
 import { AuthProvider } from "../context/AuthContext";
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-gsap.registerPlugin(ScrollTrigger);
+
 
 export default function App({ Component, pageProps, router }) {
-  useEffect(() => {
-    // Normalize scroll on mobile for smoother GSAP scroll-based animations
-    ScrollTrigger.normalizeScroll(true);
-  }, []);
+
+ 
 
   return (
     <AuthProvider>
-      <TransitionProvider>
-        <Transition>
-          <Component key={router.route} {...pageProps} />
-        </Transition>
-      </TransitionProvider>
-    </AuthProvider>
-  );
+    <TransitionProvider>
+      <Transition>
+        <Component key={router.route} {...pageProps} />
+      </Transition>
+    </TransitionProvider>
+  </AuthProvider>
+  )
 }
