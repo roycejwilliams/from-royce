cd4de5c07a0c6d00a373d83c33b49bfc8b1e482a
Royce Williams <roycewilliamsj@gmail.com>
2025-04-16 12:24:21 -0700

    update for twitter card

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -9,9 +9,20 @@ import { PostProvider } from '../context/PostContext';
 export default function App({ Component, pageProps, router }) {
   return (
     <>
-      <Head>
-        <link rel="icon" href="/public/favicon.ico" />
-      </Head>
+    
+    <Head>
+      <title>Royce – Portfolio</title>
+      <meta name="description" content="The digital home of Royce – Designer & Engineer." />
+
+      <meta property="og:title" content="Royce – Portfolio" />
+      <meta property="og:description" content="The digital home of Royce – Designer & Engineer." />
+      <meta property="og:image" content="https://from-royce.com/cover.png" />
+      <meta property="og:url" content="https://from-royce.com" />
+      <meta property="og:type" content="website" />
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:image" content="https://from-royce.com/cover.png" />
+    </Head>
 
       <AuthProvider>
         <PostProvider>
