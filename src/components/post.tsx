5110beb57b5e748808799023462ac4323d94206d
Royce Williams <roycewilliamsj@gmail.com>
2025-09-14 14:26:51 -0700

    fix linting issues

--- src/components/post.tsx
+++ src/components/post.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { usePosts } from "../hooks/api";
